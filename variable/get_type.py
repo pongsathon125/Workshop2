ec61b0a5464b17a94af3f7e233166298a82d8f73
pongsathon125 <dmtyb61@gmail.com>
2021-01-10 15:06:44 +0700

    check type

---
+++ variable/get_type.py
@@ -0,0 +1,20 @@
+name = "Pongsathon Payakasemsopon"  # string
+age = 20  # intiger
+Weight = 55  # float
+can_walk = True  # true/false
+can_run = False  # true/false
+is_dog = False  # true/false
+
+print(name)
+print(age)
+print(Weight)
+print(can_walk)
+print(can_run)
+print(is_dog)
+
+print(type(name))
+print(type(age))
+print(type(Weight))
+print(type(can_walk))
+print(type(can_run))
+print(type(is_dog))
