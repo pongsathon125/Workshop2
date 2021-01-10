3ab116141e4561617c086d438c3e399679b7ed39
pongsathon125 <dmtyb61@gmail.com>
2021-01-10 15:06:17 +0700

    variable

---
+++ variable/create.py
@@ -0,0 +1,12 @@
+name = "Pongsathon Payakasemsopon"  # string
+age = 20  # intiger
+Weight = 55  # float
+can_walk = True  # true/false
+can_run = False  # true/false
+is_dog = False  # true/false
+print(name)
+print(age)
+print(Weight)
+print(can_walk)
+print(can_run)
+print(is_dog)
