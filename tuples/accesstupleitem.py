675789b04fb107eaaf457817ea8209a1a3429496
pongsathon125 <dmtyb61@gmail.com>
2021-01-17 15:15:00 +0700

    add tuples

---
+++ tuples/accesstupleitem.py
@@ -0,0 +1,20 @@
+thislist = ["apple", "banana", "cherry"]
+
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
+
+# Output : apple
+# Output : banana
+# Output : cherry
+
+print(thislist[-1])
+print(thislist[-2])
+print(thislist[-3])
+
+# Output : cherry
+# Output : banana
+# Output : apple
+
+print(thislist[1:])
+# Output : ['banana', 'cherry']
---
+++ tuples/unpacktuple.py
@@ -0,0 +1,11 @@
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# Output :
+# apple
+# banana
+# cherry
