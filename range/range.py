adf15bb1ad82bb1fc6fef47c1bd5c8c0f06cce20
pongsathon125 <dmtyb61@gmail.com>
2021-02-20 15:55:22 +0700

    add allthing

---
+++ range/range.py
@@ -0,0 +1,11 @@
+number_list = range(6)
+print(number_list)
+# range(0,1,2,3,4,5)
+
+number_list = range(2, 6)
+print(number_list)
+# [2,3,4,5]
+
+number_list = range(3, 37, 3)  # 3 ถึงก่อน 37 เพิ่มทีละ3
+print(number_list)
+# [3,6,9,12,15,18,21,24,27,30]
