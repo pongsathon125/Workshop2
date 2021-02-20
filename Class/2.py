ef8fa5ec463226063822853e7dae6ccb54549359
pongsathon125 <dmtyb61@gmail.com>
2021-02-20 15:40:23 +0700

    Work3

---
+++ Class/2.py
@@ -0,0 +1,10 @@
+name = "pongsathon payakasemsopon"
+age = 20
+address = "Chachoengsao"
+
+Person = Person(name, age, address)
+
+print(Person.get_name())
+print(Person.get_age())
+print(Person.get_address())
+print(Person.info())
